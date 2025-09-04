19013fb3e811e013a41a8806379222cc0404fc9c
Ilyas <tdw.vendor1@gmail.com>
2025-09-04 19:03:36 +0700

    add echo v4 middleware for logger and recover

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	// Initialize Echo
 	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Logger(), middleware.Recover())
 
 	// Health check endpoint
 	e.GET("/health", func(c echo.Context) error {
